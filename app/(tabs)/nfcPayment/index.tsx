941789fb84d7d5b0201b59db12005c90e5b87c6e
TheOne001 <paul.agbogun@gmail.com>
2025-03-30 22:37:40 +0100

    Fix improper use of react lifecycle

--- app/(tabs)/nfcPayment/index.tsx
+++ app/(tabs)/nfcPayment/index.tsx
@@ -1,4 +1,4 @@
-import { Alert, Modal, Pressable, StyleSheet, Text, View} from "react-native";
+import { Alert, Modal, Pressable, StyleSheet, Text, View } from "react-native";
 import React, { FC, useEffect, useRef, useState, useCallback } from "react";
 import { Colors } from "@/constants/Colors";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -7,93 +7,93 @@ import ShareIC from "@/assets/images/svg/ShareIC";
 import { router, useLocalSearchParams } from "expo-router";
 import { hp } from "@/helpers/common";
 import LottieView from "lottie-react-native";
-import { BlurView } from 'expo-blur';
-import NfcManager, {NfcTech} from 'react-native-nfc-manager';
-
-// Pre-step, call this before any NFC operations
-NfcManager.start();
-
+import { BlurView } from "expo-blur";
+import NfcManager, { NfcTech } from "react-native-nfc-manager";
 
 const NfcPaymentScreen: FC = () => {
   const { top } = useSafeAreaInsets();
   const paddinTop = top > 0 ? top + 10 : 30;
   const animation = useRef<LottieView>(null);
-  const { amount, narrattion } = useLocalSearchParams(); 
+  const { amount, narrattion } = useLocalSearchParams();
   const [modalVisible, setModalVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Pre-step, call this before any NFC operations
+    NfcManager.start();
+  }, []);
 
   useEffect(
     useCallback(() => {
       // This will run when the screen is focused (i.e. user navigates back)
-    
+
       const readNdef = async () => {
         try {
           // register for the NFC tag with NDEF in it
           await NfcManager.requestTechnology(NfcTech.Ndef);
           // the resolved tag object will contain `ndefMessage` property
           const tag = await NfcManager.getTag();
-          Alert.alert('Payment sucessful!');
-          router.navigate('/(tabs)/reciept');
+          Alert.alert("Payment sucessful!");
+          router.navigate("/(tabs)/reciept");
 
           // console.warn('Tag found', tag);
         } catch (ex) {
-          Alert.alert('Oops!, A Certain Error Occured!');
+          Alert.alert("Oops!, A Certain Error Occured!");
           router.back();
-    
+
           //console.warn('Oops!', ex);
         } finally {
           // stop the nfc scanning
           NfcManager.cancelTechnologyRequest();
         }
       };
-    
+
       if (amount && Number(amount) > 15000) {
         setModalVisible(true);
-      }else{
+      } else {
         readNdef();
-      };
-  
+      }
+
       // Cleanup if necessary (this runs when the screen is unfocused)
       return () => {
         // Any cleanup logic, if needed
-       setModalVisible(false);
+        setModalVisible(false);
       };
     }, [amount])
-  ), [amount];
-  
-const handleModalClose: ()=>void = () => {
+  ),
+    [amount];
+
+  const handleModalClose: () => void = () => {
     setModalVisible(!modalVisible);
-    router.navigate('/(tabs)/nfcPayment/paymentVerification');
-    
-}
+    router.navigate("/(tabs)/nfcPayment/paymentVerification");
+  };
 
-
-
-  
   return (
     <View style={[styles.container, { paddingTop: paddinTop }]}>
-        {/* Modal */}
-        
-        <Modal
+      {/* Modal */}
+
+      <Modal
         animationType="fade"
         transparent={true}
         visible={modalVisible}
-        
         onRequestClose={() => {
           // Alert.alert('Modal has been closed.');
           setModalVisible(!modalVisible);
-        }}>
-          <BlurView intensity={30} tint="dark" style={styles.centeredView}>
+        }}
+      >
+        <BlurView intensity={30} tint="dark" style={styles.centeredView}>
           <View style={styles.modalView}>
             <Text style={styles.modalTextTitle}>Payment Limit Reached</Text>
-            <Text style={styles.modalTextDes}>Oops! Looks like you've hit the payment limit. For transactions above ₦15,000, please proceed by entering your ATM{'\n'} card PIN in the designated area.</Text>
-            <Pressable
-              style={styles.button}
-              onPress={handleModalClose}>
+            <Text style={styles.modalTextDes}>
+              Oops! Looks like you've hit the payment limit. For transactions
+              above ₦15,000, please proceed by entering your ATM{"\n"} card PIN
+              in the designated area.
+            </Text>
+            <Pressable style={styles.button} onPress={handleModalClose}>
               <Text style={styles.textStyle}>Proceed</Text>
             </Pressable>
           </View>
         </BlurView>
-        </Modal>
+      </Modal>
       {/* Buttons */}
       <View style={styles.topBtn}>
         <Pressable onPress={() => router.back()}>
@@ -192,24 +192,24 @@ const styles = StyleSheet.create({
   },
   modalTextTitle: {
     marginBottom: 15,
-    fontFamily: 'Monserrat-Regular',
-    fontWeight: '600',
-    textAlign: 'center',
+    fontFamily: "Monserrat-Regular",
+    fontWeight: "600",
+    textAlign: "center",
     fontSize: 20,
     color: Colors.main.text,
   },
   modalTextDes: {
     marginBottom: 15,
-    fontFamily: 'Raleway-Regular',
-    textAlign: 'center',
+    fontFamily: "Raleway-Regular",
+    textAlign: "center",
     lineHeight: 30,
     fontSize: 15,
     color: Colors.main.text,
   },
   centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     //marginTop: 22,
     //backgroundColor: 'rgba(0, 0, 0, 0.4)',
   },
@@ -219,8 +219,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 40,
     gap: 10,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -237,10 +237,10 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.main.primary,
   },
   textStyle: {
-    color: 'white',
-    fontFamily: 'Raleway-Regular',
+    color: "white",
+    fontFamily: "Raleway-Regular",
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
