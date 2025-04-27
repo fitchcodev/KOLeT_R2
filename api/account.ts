044bae6d739779f431abcab618b1d7202308c9e3
Ehbraheem <ehbraheem@gmail.com>
2025-04-27 21:18:15 +0100

    Change url for get bank list.

--- api/account.ts
+++ api/account.ts
@@ -1,27 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
-import { useMutation } from "@tanstack/react-query";
-import axios, { AxiosError } from "axios";
+import { useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
+import axios, { AxiosError } from 'axios';
 
 const API_URL = process.env.EXPO_PUBLIC_BASE_URL;
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 export const useAddAccount = (onSuccess, onError) => {
   const requestFn = async (data) => {
     try {
-      const response = await api.post("/add1.php", data);
+      const response = await api.post('/add1.php', data);
       return response.data;
     } catch (err) {
       const error = err as AxiosError | Error | any;
-      console.error("API Error:", error);
+      console.error('API Error:', error);
       throw new Error(
         error.response?.data?.message ||
-          `Error: ${error.message || "Unknown error occurred"}`
+          `Error: ${error.message || 'Unknown error occurred'}`
       );
     }
   };
@@ -29,11 +29,11 @@ export const useAddAccount = (onSuccess, onError) => {
   const mutation = useMutation({
     mutationFn: requestFn,
     onSuccess: (data) => {
-      console.log("Account added successfully:", data);
+      console.log('Account added successfully:', data);
       if (onSuccess) onSuccess(data);
     },
     onError: (error) => {
-      console.error("Error adding account:", error);
+      console.error('Error adding account:', error);
       if (onError) onError(error);
     },
   });
@@ -43,14 +43,14 @@ export const useAddAccount = (onSuccess, onError) => {
 export const useGetAccountName = () => {
   const requestFn = async (data) => {
     try {
-      const response = await api.post("/add2.php", data);
+      const response = await api.post('/add2.php', data);
       return response.data;
     } catch (err) {
       const error = err as AxiosError | Error | any;
-      console.error("API Error:", error);
+      console.error('API Error:', error);
       throw new Error(
         error.response?.data?.message ||
-          `Error: ${error.message || "Unknown error occurred"}`
+          `Error: ${error.message || 'Unknown error occurred'}`
       );
     }
   };
@@ -58,10 +58,10 @@ export const useGetAccountName = () => {
   const mutation = useMutation({
     mutationFn: requestFn,
     onSuccess: (data) => {
-      console.log("Account found successfully", data);
+      console.log('Account found successfully', data);
     },
     onError: (error) => {
-      console.error("Error finding account:", error);
+      console.error('Error finding account:', error);
     },
   });
 
@@ -69,19 +69,19 @@ export const useGetAccountName = () => {
 };
 
 export const useGetBankList = (options = {}) => {
-  const queryKey = ["bankList"];
+  const queryKey = ['bankList'];
 
   const fetchBankList = async () => {
     try {
-      const response = await api.get("/get_bank_list_paystack.php");
+      const response = await axios.get('https://api.paystack.co/bank');
 
-      return response.data.response.data;
+      return response.data;
     } catch (err) {
       const error = err as AxiosError | Error | any;
-      console.error("API Error:", error);
+      console.error('API Error:', error);
       throw new Error(
         error.response?.data?.message ||
-          `Error: ${error.message || "Failed to fetch bank list"}`
+          `Error: ${error.message || 'Failed to fetch bank list'}`
       );
     }
   };
